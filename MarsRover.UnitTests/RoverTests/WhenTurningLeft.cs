9bee531f62f82696bca5397f5bc287a9414eabd8
Cameron Presley <Cameron@TheSoftwareMentor.com>
2020-05-09 13:07:14 -0400

    Added test cases for when turning left

--- MarsRover.UnitTests/RoverTests/WhenTurningLeft.cs
+++ MarsRover.UnitTests/RoverTests/WhenTurningLeft.cs
@@ -2,19 +2,23 @@ using NUnit.Framework;
 
 namespace MarsRover.UnitTests.RoverTests
 {
-    [TestFixture]
-    public class WhenTurningLeft
+  [TestFixture]
+  public class WhenTurningLeft
+  {
+    [Test]
+    [TestCase(Direction.North, Direction.West, TestName = "AndFacingNorthThenTheRoverFacesWest")]
+    [TestCase(Direction.West, Direction.South, TestName = "AndFacingWestThenTheRoverFacesSouth")]
+    [TestCase(Direction.South, Direction.East, TestName = "AndFacingSouthThenTheRoverFacesEast")]
+    [TestCase(Direction.East, Direction.North, TestName = "AndFacingEastThenTheRoverFacesNorth")]
+    public void RoverTurningLeft(Direction start, Direction expected)
     {
-        [Test]
-        public void AndFacingNorthThenRoverFacesWest()
-        {
-            var rover = new Rover{Orientation=Direction.North};
-            var initialLocation = rover.Location;
+      var rover = new Rover { Orientation = start };
+      var initialLocation = rover.Location;
 
-            rover.TurnLeft();
+      rover.TurnLeft();
 
-            Assert.AreEqual(Direction.West, rover.Orientation);
-            Assert.AreEqual(initialLocation, rover.Location);
-        }
+      Assert.AreEqual(expected, rover.Orientation);
+      Assert.AreEqual(initialLocation, rover.Location);
     }
+  }
 }
